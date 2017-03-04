f1e51a9f2ed86fcd506108f7cc5db157e6339fcb
Alexander Lewis <AceLewis@users.noreply.github.com>
2017-03-04 16:04:58 +0000

    Slight improvments to the positional solution

--- Rotate-your-vowels/positional_solution.py
+++ Rotate-your-vowels/positional_solution.py
@@ -1,5 +1,5 @@
 string = 'The quick brown fox jumps over the lazy dog'
 
 for x in range(20):
-    string = ''.join(['eiouaEIOUA' + c][0]['aeiouAEIOU'.find(c)] for c in string)
+    string = ''.join(('eiouaEIOUA' + c)['aeiouAEIOU'.find(c)] for c in string)
     print(string)
