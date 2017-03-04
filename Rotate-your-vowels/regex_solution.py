757a5a8fd024ceffe2c659bf440b839e19101e97
Alexander Lewis <AceLewis@users.noreply.github.com>
2017-03-04 15:43:32 +0000

    Broke up function

--- Rotate-your-vowels/regex_solution.py
+++ Rotate-your-vowels/regex_solution.py
@@ -1,7 +1,8 @@
 import re
 
 string = 'The quick brown fox jumps over the lazy dog'
+sub_function = lambda x: 'eiouaEIOUA'['aeiouAEIOU'.find(x.group(0))]
 
 for x in range(20):
-    string = re.sub('(?i)[aeiou]', lambda x: 'eiouaEIOUA'['aeiouAEIOU'.find(x.group(0))], string)
+    string = re.sub('(?i)[aeiou]', sub_function, string)
     print(string)
