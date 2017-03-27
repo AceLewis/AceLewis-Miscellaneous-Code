7e08a898730bfa0fb1ee5c373fe687625d6226ac
Alexander Lewis <AceLewis@users.noreply.github.com>
2017-03-27 18:03:22 +0100

    Tab to spaces repl.it auto used tab

--- HipsterPy/Hipster.py
+++ HipsterPy/Hipster.py
@@ -1,7 +1,7 @@
 import re
 
 def hipsterpy(string):
-	return re.sub(r"(?i)([^\s])[aeiou]([^aeiou]*)\b", r'\1\2', string)
+    return re.sub(r"(?i)([^\s])[aeiou]([^aeiou]*)\b", r'\1\2', string)
 
 strings = "A Hipster follows the latest trends and fashions."
 
