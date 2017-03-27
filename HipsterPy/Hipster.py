e45ce00c60f460a3571ab5963679d2258494fd66
Alexander Lewis <AceLewis@users.noreply.github.com>
2017-03-27 17:57:33 +0100

    Neatened string (before I used multiline paragraph)

--- HipsterPy/Hipster.py
+++ HipsterPy/Hipster.py
@@ -3,8 +3,6 @@ import re
 def hipsterpy(string):
 	return re.sub(r"(?i)([^\s])[aeiou]([^aeiou]*)\b", r'\1\2', string)
 
-strings = """
-A Hipster follows the latest trends and fashions.
-""".strip()
+strings = "A Hipster follows the latest trends and fashions."
 
 print(hipsterpy(strings))
